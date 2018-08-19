dea4ba69bb40847daee2857ffc90e18e18020aa3
Carolina Vasconcelos <carol.vasconcelos080@gmail.com>
2018-08-19 19:52:35 -0300

    Criação do componente UserForm e inclusão de usuários

--- src/components/user/UserCrud.jsx
+++ src/components/user/UserCrud.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Main from '../template/Main'
+import UserForm from './Userform'
+import axios from 'axios'
 
 const headerProps = {
     icon: 'users',
@@ -7,11 +9,67 @@ const headerProps = {
     subtitle: 'Cadastro de Usuários: Incluir, Listar, Alterar e Excluir'
 }
 
+const baseUrl = 'http://localhost:3001/users'
+
+const initialState = {
+    user: {
+        name: '',
+        email: ''
+    },
+    list: []
+}
+
 export default class UserCrud extends React.Component {
+
+    constructor() {
+        super()
+
+        this.state = { ...initialState }
+
+        this.clear = this.clear.bind(this)
+        this.save = this.save.bind(this)
+        this.getUpdatedList = this.getUpdatedList.bind(this)
+        this.updateField = this.updateField.bind(this)
+    }
+
+    clear() {
+        this.setState({ user: initialState.user })
+    }
+
+    save() {
+        const user = this.state.user
+        const method = user.id ? 'put' : 'post'
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+
+        axios[method](url, user)
+            .then(response => {
+                const list = this.getUpdatedList(response.data)
+                this.setState({ user: initialState.user, list })
+            })
+
+    }
+
+    getUpdatedList(user) {
+        const list = this.state.list.filter(u => u.id !== user.id)
+        list.unshift(user)
+        return list
+    }
+
+    updateField(event) {
+        const user = { ...this.state.user }
+        user[event.target.name] = event.target.value
+        this.setState({ user })
+    }
+
     render() {
         return (
             <Main {...headerProps}>
-                Cadastro de Usuários
+                <UserForm name={this.state.user.name}
+                    email={this.state.user.email}
+                    clear={this.clear}
+                    save={this.save}
+                    updateField={this.updateField}
+                />
             </Main>
         )
     }
